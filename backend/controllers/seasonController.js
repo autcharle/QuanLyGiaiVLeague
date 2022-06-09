006074a6afb749c9b502c960315830d2f02dc362
PLHPhuong <64248912+PLHPhuong@users.noreply.github.com>
2022-06-09 21:37:36 +0700

    fix seasonController

--- backend/controllers/seasonController.js
+++ backend/controllers/seasonController.js
@@ -11,51 +11,84 @@ const createSeason = asyncHandler(async (req, res) => {
   //   res.status(400);
   //   throw new Error("Please add a text field");
   // }
-  console.log(req.body)
-  const name = req.body.name || "Vleague " + new Date().getFullYear().toString();
+
+  const name = req.body.name
+    ? req.body.name
+    : "Vleague " + new Date().getFullYear().toString();
   const seasonExist = await Season.findOne({ name });
   if (seasonExist) {
     // res.status(400);
     // throw new Error("Already created for this season");
-    console.log(name)
-    res.status(200).json({message:'Already created for this season',seasonExist})
+    res
+      .status(200)
+      .json({ message: "Already created for this season", seasonExist });
     return;
   }
 
   const season = await Season.create({
     name: name,
-    number_of_player: {
-      min_player: req.body.min_player || 15,
-      max_player: req.body.max_player || 22,
-    },
-    number_of_foreign_player: {
-      min_player: req.body.min_foreign_player || 0,
-      max_player: req.body.max_foreign_player || 3,
-    },
-    age: {
-      min_age: req.body.min_age || 16,
-      max_age: req.body.max_age || 40,
-    },
-    play_duration: req.body.play_duration || 96,
-    start_date:  Date.parse(req.body.start_date) || null,
-    end_date: Date.parse(req.body.end_date) || null,
-    win_point: req.body.win_point || 3,
-    draw_point: req.body.draw_point || 1,
-    lose_point: req.body.lose_point || 0,
-    goal_difference_rank: req.body.goal_difference_rank || 1,
-    point_rank: req.body.point_rank || 2,
-    win_rank: req.body.win_rank || 3,
-    draw_rank: req.body.draw_rank || 4,
-    lose_rank: req.body.lose_rank || 5,
-    goal_type: req.body.goal_type || [
-      A,
-      B,
-      C,
-    ],
-    player_type:req.body.player_type || [
-      native,
-      foreign
-    ],
+
+    min_player:
+      typeof req.body.min_player !== "undefined" ? req.body.min_player : 15,
+
+    max_player:
+      typeof req.body.max_player !== "undefined" ? req.body.max_player : 22,
+
+    min_foreign_player:
+      typeof req.body.min_foreign_player !== "undefined"
+        ? req.body.min_foreign_player
+        : 0,
+
+    max_foreign_player:
+      typeof req.body.max_foreign_player !== "undefined"
+        ? req.body.max_foreign_player
+        : 3,
+
+    min_age: typeof req.body.min_age !== "undefined" ? req.body.min_age : 16,
+
+    max_age: typeof req.body.max_age !== "undefined" ? req.body.max_age : 40,
+
+    play_duration:
+      typeof req.body.play_duration !== "undefined"
+        ? req.body.play_duration
+        : 96,
+
+    start_date:
+      req.body.start_date
+        ? Date.parse(req.body.start_date)
+        : null,
+
+    end_date:
+      req.body.end_date ? Date.parse(req.body.end_date) : null,
+
+    win_point:
+      typeof req.body.win_point !== "undefined" ? req.body.win_point : 3,
+
+    draw_point:
+      typeof req.body.draw_point !== "undefined" ? req.body.draw_point : 1,
+
+    lose_point:
+      typeof req.body.lose_point !== "undefined" ? req.body.lose_point : 0,
+
+    goal_difference_rank: req.body.goal_difference_rank
+      ? req.body.goal_difference_rank
+      : 1,
+
+    point_rank:
+      typeof req.body.point_rank !== "undefined" ? req.body.point_rank : 2,
+
+    win_rank: typeof req.body.win_rank !== "undefined" ? req.body.win_rank : 3,
+
+    draw_rank:
+      typeof req.body.draw_rank !== "undefined" ? req.body.draw_rank : 4,
+
+    lose_rank:
+      typeof req.body.lose_rank !== "undefined" ? req.body.lose_rank : 5,
+
+    goal_type:
+    Array.isArray(req.body.goal_type) && req.body.goal_type.length != 0
+        ? req.body.goal_type
+        : ["A", "B", "C"],
   });
   res.status(200).json(season);
 });
@@ -81,7 +114,9 @@ const findSeason = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please enter name field");
   }
-  const seasons = await Season.find({name :  { $regex: '.*' + req.body.name + '.*' }});
+  const seasons = await Season.find({
+    name: { $regex: ".*" + req.body.name + ".*" },
+  });
   res.status(200).json(seasons);
 });
 // @desc    Update season
@@ -89,55 +124,122 @@ const findSeason = asyncHandler(async (req, res) => {
 // @access  Public
 const updateSeason = asyncHandler(async (req, res) => {
   const season = await Season.findById(req.params.id);
-  if (!season){
+  if (!season) {
     res.status(400);
     throw new Error("Season not exists");
   }
 
   const updateValue = {
-    name: req.body.name || season.name,
-    number_of_player: {
-      min_player: req.body.min_player || season.number_of_player.min_player,
-      max_player: req.body.max_player || season.number_of_player.max_player,
-    },
-    number_of_foreign_player: {
-      min_player: req.body.min_foreign_player || season.number_of_foreign_player.min_player,
-      max_player: req.body.max_foreign_player || season.number_of_foreign_player.max_player,
-    },
-    age: {
-      min_age: req.body.min_age || season.age.min_age,
-      max_age: req.body.max_age || season.age.max_age,
-    },
-    play_duration: req.body.play_duration || season.play_duration,
-    start_date:  Date.parse(req.body.start_date) || season.start_date,
-    end_date: Date.parse(req.body.end_date) || season.end_date,
-    win_point: req.body.win_point || season.win_point,
-    draw_point: req.body.draw_point || season.draw_point,
-    lose_point: req.body.lose_point || season.lose_point,
-    goal_difference_rank: req.body.goal_difference_rank || season.goal_difference_rank,
-    point_rank: req.body.point_rank || season.point_rank ,
-    win_rank: req.body.win_rank || season.win_rank,
-    draw_rank: req.body.draw_rank || season.draw_rank,
-    lose_rank: req.body.lose_rank || season.lose_rank,
-    goal_type: req.body.goal_type || season.goal_type,
-    player_type:req.body.player_type || season.player_type,
-  }
-  const updatedSeason = await Season.findByIdAndUpdate(req.params.id, updateValue, {
-    new: true,
-  });
-  res.status(200).json({ msg: "updateSeason" ,updatedSeason});
+    name: req.body.name ? req.body.name : season.name,
+
+    min_player:
+      typeof req.body.min_player !== "undefined"
+        ? req.body.min_player
+        : season.min_player,
+
+    max_player:
+      typeof req.body.max_player !== "undefined"
+        ? req.body.max_player
+        : season.max_player,
+
+    min_foreign_player:
+      typeof req.body.min_foreign_player !== "undefined"
+        ? req.body.min_foreign_player
+        : season.min_foreign_player,
+
+    max_foreign_player:
+      typeof req.body.max_foreign_player !== "undefined"
+        ? req.body.max_foreign_player
+        : season.max_foreign_player,
+
+    min_age:
+      typeof req.body.min_age !== "undefined"
+        ? req.body.min_age
+        : season.age.min_age,
+
+    max_age:
+      typeof req.body.max_age !== "undefined"
+        ? req.body.max_age
+        : season.age.max_age,
+
+    play_duration:
+      typeof req.body.play_duration !== "undefined"
+        ? req.body.play_duration
+        : season.play_duration,
+
+    start_date: req.body.start_date
+      ? Date.parse(req.body.start_date)
+      : season.start_date,
+
+    end_date: req.body.end_date
+      ? Date.parse(req.body.end_date)
+      : season.end_date,
+
+    win_point:
+      typeof req.body.win_point !== "undefined"
+        ? req.body.win_point
+        : season.win_point,
+
+    draw_point:
+      typeof req.body.draw_point !== "undefined"
+        ? req.body.draw_point
+        : season.draw_point,
+
+    lose_point:
+      typeof req.body.lose_point !== "undefined"
+        ? req.body.lose_point
+        : season.lose_point,
+
+    goal_difference_rank:
+      typeof req.body.goal_difference_rank !== "undefined"
+        ? req.body.goal_difference_rank
+        : season.goal_difference_rank,
+
+    point_rank:
+      typeof req.body.point_rank !== "undefined"
+        ? req.body.point_rank
+        : season.point_rank,
+
+    win_rank:
+      typeof req.body.win_rank !== "undefined"
+        ? req.body.win_rank
+        : season.win_rank,
+
+    draw_rank:
+      typeof req.body.draw_rank !== "undefined"
+        ? req.body.draw_rank
+        : season.draw_rank,
+
+    lose_rank:
+      typeof req.body.lose_rank !== "undefined"
+        ? req.body.lose_rank
+        : season.lose_rank,
+
+    goal_type:
+      Array.isArray(req.body.goal_type) && req.body.goal_type.length !=0
+        ? req.body.goal_type
+        : season.goal_type,
+  };
+  const updatedSeason = await Season.findByIdAndUpdate(
+    req.params.id,
+    updateValue,
+    {
+      new: true,
+    }
+  );
+  res.status(200).json({ msg: "updateSeason", updatedSeason });
 });
 // @desc    Delete season
 // @route   DELETE /api/seasons:id
 // @access  Public
 const deleteSeason = asyncHandler(async (req, res) => {
   const season = await Season.findById(req.params.id);
-  if (!season){
+  if (!season) {
     res.status(400);
     throw new Error("Season not exists");
   }
   await season.remove();
-  res.status(200).json({id : req.params.id });
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
--- backend/models/seasonModel.js
+++ backend/models/seasonModel.js
@@ -1,97 +1,93 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const seasonSchema = mongoose.Schema({
+const seasonSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Please add a name']
+      type: String,
+      required: [true, "Please add a name"],
     },
-    number_of_player:{
-        min_player:{
-            type: Number,
-            default: 15
-        },
-        max_player:{
-            type: Number,
-            default: 22
-        },
+    min_player: {
+      type: Number,
+      default: 15,
     },
-    number_of_foreign_player:{
-        min_player:{
-            type: Number,
-            default: 0
-        },
-        max_player:{
-            type: Number,
-            default: 3
-        },
+    max_player: {
+      type: Number,
+      default: 22,
     },
-    age:{
-        min_age:{
-            type: Number,
-            default: 16
-        },
-        max_age:{
-            type: Number,
-            default: 40
-        },
+    min_foreign_player: {
+      type: Number,
+      default: 0,
     },
-    play_duration:{
-        type: Number,
-        default: 96
+    max_foreign_player: {
+      type: Number,
+      default: 3,
     },
-    play_duration:{
-        type: Number,
-        default: 96
+    min_age: {
+      type: Number,
+      default: 16,
     },
-    start_date:{
-        type: Date,
+    max_age: {
+      type: Number,
+      default: 40,
+    },
+    play_duration: {
+      type: Number,
+      default: 96,
+    },
+    play_duration: {
+      type: Number,
+      default: 96,
+    },
+    start_date: {
+      type: Date,
     },
     end_date: {
-        type: Date,
+      type: Date,
     },
-    win_point:{
-        type: Number,
-        default: 3
+    win_point: {
+      type: Number,
+      default: 3,
     },
-    draw_point:{
-        type: Number,
-        default:1
+    draw_point: {
+      type: Number,
+      default: 1,
     },
-    lose_point:{
-        type: Number,
-        default: 0
-    }, 
-    goal_difference_rank :
-    {
-        type: Number,
-        default: 1,
-    },  
-    point_rank:{
-        type: Number,
-        default: 2,
+    lose_point: {
+      type: Number,
+      default: 0,
     },
-    win_rank:{
-        type: Number,
-        default: 3,
+    goal_difference_rank: {
+      type: Number,
+      default: 1,
+    },
+    point_rank: {
+      type: Number,
+      default: 2,
+    },
+    win_rank: {
+      type: Number,
+      default: 3,
     },
     draw_rank: {
-        type: Number,
-        default: 4,
+      type: Number,
+      default: 4,
     },
     lose_rank: {
-        type: Number,
-        default: 5,
-    },   
-    goal_type:{
-        type: [String],
-        default: ['A','B','C']
+      type: Number,
+      default: 5,
+    },
+    goal_type: {
+      type: [String],
+      default: ["A", "B", "C"],
     },
     // player_type:{
     //     type: [String],
     //     default: ['native','foreign']
     // }
-}, {
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-module.exports = mongoose.model('Season', seasonSchema)
+module.exports = mongoose.model("Season", seasonSchema);
